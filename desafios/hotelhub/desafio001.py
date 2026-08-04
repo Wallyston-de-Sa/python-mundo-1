f5c2b108c5ec15b6307f4844946040e57faaedf5
Wallyston <wallyston98@gmail.com>
2026-08-04 13:52:21 -0300

    hotelhub01

---
+++ desafios/hotelhub/desafio001.py
@@ -0,0 +1,7 @@
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+cidade = input('Qual cidade você mora? ')
+
+print('===== HOTELHUB =====\n')
+print('Hóspede cadastrado com sucesso!\n')
+print('Nome:', nome, '\nIdade:', idade, 'anos\nCidade:', cidade)
