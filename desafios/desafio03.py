f093c3c14bade5d379e8ff9a07b81db98068db05
Wallyston <wallyston98@gmail.com>
2026-08-04 13:32:55 -0300

    desafios

---
+++ desafios/desafio03.py
@@ -0,0 +1,4 @@
+#Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+print('A soma é', (num1+num2))
