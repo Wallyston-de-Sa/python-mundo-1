d62114770e1885bf3ff9ef0c01e987cf7ced16c0
Wallyston <wallyston98@gmail.com>
2026-08-08 00:04:52 -0300

    Update desafio013.py

--- desafios/curso/desafio013.py
+++ desafios/curso/desafio013.py
@@ -2,4 +2,4 @@
 salario_funcionario = float(input('Qual é o seu salário? R$'))
 aumento = salario_funcionario * (15/100)
 salario_final = salario_funcionario + aumento
-print('Seu salário de R${:.2f} com um aumento de 15% passará a ser R${:.2f}'.format(salario_funcionario, salario_final))
+print('Seu salário de R${:.2f} com um aumento de 15%. Sera de R${:.2f}'.format(salario_funcionario, salario_final))
